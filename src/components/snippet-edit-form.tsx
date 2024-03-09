ec25f6ff635ed1fc2a53db0778cc39f280cc369f
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-09 17:12:02 +0100

    Add editSnippetAction server function

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,6 +2,7 @@
 import {Editor} from '@monaco-editor/react'
 import type {Snippet} from '@prisma/client'
 import {useState} from 'react'
+import * as actions from '@/actions'
 
 interface SnippetEditFormProps {
     snippet: Snippet
@@ -13,6 +14,8 @@ const SnippetEditForm = ({snippet}: SnippetEditFormProps) => {
     const handleEditorChange = (value: string = '') => {
         setCode(value)
     }
+
+    const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code)
 
     return (
         <div>
@@ -28,6 +31,11 @@ const SnippetEditForm = ({snippet}: SnippetEditFormProps) => {
                 }}
                 onChange={handleEditorChange}
             />
+            <form action={editSnippetAction}>
+                <button type="submit" className="p-2 border rounded">
+                    Save
+                </button>
+            </form>
         </div>
     )
 }
