cb0deb04b9bba00c6a0586940735f667e1a80f49
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-09 19:48:15 +0100

    Add createSnippet server action function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,3 +19,48 @@ export async function deleteSnippet(id: number) {
 
     redirect('/')
 }
+
+export async function createSnippet(
+    formState: {message: string},
+    formData: FormData
+) {
+    try {
+        // Check the user's input and make sure they are valid
+        const title = formData.get('title')
+        const code = formData.get('code')
+
+        if (typeof title !== 'string' || title.length < 3) {
+            return {
+                message: 'Title must be longer',
+            }
+        }
+        if (typeof code !== 'string' || code.length < 3) {
+            return {
+                message: 'Code must be longer',
+            }
+        }
+
+        // Create a new record in the database
+        await db.snippet.create({
+            data: {
+                title,
+                code,
+            },
+        })
+
+        // throw new Error('Failed to save to database')
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                message: error.message,
+            }
+        } else {
+            return {
+                message: 'Something went wrong',
+            }
+        }
+    }
+
+    // Redirect the user back to the root route. Never put a redirect in a try/catch block
+    redirect('/')
+}
