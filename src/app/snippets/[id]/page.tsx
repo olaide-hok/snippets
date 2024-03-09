3dd26b9bfd552d7534a9d85c2b9619a8433d4755
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-09 20:32:25 +0100

    Add caching for dynamic routes

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -44,3 +44,13 @@ const SnippetShowPage = async (props: SnippetShowPageProps) => {
 }
 
 export default SnippetShowPage
+
+export async function generateStaticParams() {
+    const snippets = await db.snippet.findMany()
+
+    return snippets.map((snippet) => {
+        return {
+            id: snippet.id.toString(),
+        }
+    })
+}
